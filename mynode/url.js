f8f87e6f266e06b0d2008986514e674d533f7444
erluzi <747877078@qq.com>
2018-07-02 11:34:17 +0800

    fix

--- mynode/url.js
+++ mynode/url.js
@@ -2,7 +2,7 @@ const {URL} = require('url')
 const myURL =
     new URL('https://user:pass@sub.host.com:8080/p/a/t/h?query=string#hash');
 
-console.log(myURL)
+console.log(myURL);
 
 /*
  URL {
--- node/webpush/testPush.js
+++ node/webpush/testPush.js
@@ -9,8 +9,10 @@ const endpointOfChrome =
     'https://fcm.googleapis.com/fcm/send/diHtVet1zz8:APA91bF4NxUJfWrx8kvSVL4WAEHbIHCgOy6-KuTH9oOcx2o68NbJQqcY00XPuVGOTNFcplwUjwijWhEX4FhQCthzPVdut6xDYXy3EleZuGjrsLwO-7Gvts6dcybCC5jsJ3jstxWWDTHo';
 const endpointOfFirefox =
     'https://updates.push.services.mozilla.com/wpush/v2/gAAAAABa_7RDyNAcXmc3bEl7usZisz-BFCQ0TxUfISA3AhKmjjnCRFkQHjR7j-RehgIzHWiq-_TPVPnochSfDCKpSE7YQI5xQpTS2-rrgjEC9K2q--w0u0sFasf2JJHrKFLzwi1fg4NNSw88nKZ1e0h50_dpc5AF3ZjLCKEAbGhL1oE6FFMne5Y';
+
 const auth = 'QdQw3LnOitj1Yvn3-mNj_g';
 const p256dh = 'BIB1xcmWZcMdbZISQKCVHtxWSZy0hgDdhaxHSYsKNBrjASEH62IGQ8zsheiQ4ztcJpLXwL_RwzaT-hSSmUUcV18';
+
 const pushMeg = {
     title: 'push title!',
 	body: 'a test push msg.',
@@ -51,7 +53,5 @@ for(let endpoint of endpointArr){
     };
     sendPush(pushSubscription, JSON.stringify(pushMeg));
 }
-
-
 
 module.exports = sendPush;
