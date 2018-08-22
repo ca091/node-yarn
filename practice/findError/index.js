794095160673c994e6befee894f612db1e48d069
erluzi <747877078@qq.com>
2018-08-22 17:06:19 +0800

    fix

--- practice/findError/index.js
+++ practice/findError/index.js
@@ -1,9 +1,13 @@
-const jsPath = 'http://sbc.tonglvhuanqiu.com/static/js/app.cde53d14d0bae3ec19c9.js';
+const jsPath = 'http://sbc.tonglvhuanqiu.com/static/js/vendor.bd3474c58bdda3752c14.js';
+// const jsPath = 'http://sbc.tonglvhuanqiu.com/static/js/app.a3ad6837401f9b5002eb.js';
 const {Api} = require('bitutilsofnode');
 const spread = 250;
-//js错误上报是压缩过的js，不方便定位错误，此方法就是为了快速定位错误
+//js错误上报是压缩过的js，不方便定位错误，此方法是为了快速定位错误
 function find({line=1, column}) {
     Api.get(jsPath).then(data => {
+        if(line !== 1){
+          data = data.split('\n')[line-1];
+        }
         let str = data.substring(column-spread, column+spread);
         let errorStr = str.substring(spread, spread*2);
         console.log(str+'\n');
@@ -11,4 +15,5 @@ function find({line=1, column}) {
     })
 }
 
-find({column: 362633});
+// find({column: 362633});
+find({line: 9, column: 66767});
