b499f33141e1f4f545a354ff6f1057ec7d8a706e
erluzi <747877078@qq.com>
2018-08-22 17:10:54 +0800

    fix: 格式化

--- practice/findError/index.js
+++ practice/findError/index.js
@@ -2,15 +2,16 @@ const jsPath = 'http://sbc.tonglvhuanqiu.com/static/js/vendor.bd3474c58bdda3752c
 // const jsPath = 'http://sbc.tonglvhuanqiu.com/static/js/app.a3ad6837401f9b5002eb.js';
 const {Api} = require('bitutilsofnode');
 const spread = 250;
+
 //js错误上报是压缩过的js，不方便定位错误，此方法是为了快速定位错误
-function find({line=1, column}) {
+function find({line = 1, column}) {
     Api.get(jsPath).then(data => {
-        if(line !== 1){
-          data = data.split('\n')[line-1];
+        if (line !== 1) {
+            data = data.split('\n')[line - 1];
         }
-        let str = data.substring(column-spread, column+spread);
-        let errorStr = str.substring(spread, spread*2);
-        console.log(str+'\n');
+        let str = data.substring(column - spread, column + spread);
+        let errorStr = str.substring(spread, spread * 2);
+        console.log(str + '\n');
         console.log(errorStr)
     })
 }
