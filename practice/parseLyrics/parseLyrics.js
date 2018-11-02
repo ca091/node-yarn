e8a1e60a44e1db2fa71e9fc71fcd8b6dc57f1aec
erluzi <747877078@qq.com>
2018-11-02 14:57:25 +0800

    fix:解析歌词-修正误差

--- practice/parseLyrics/parseLyrics.js
+++ practice/parseLyrics/parseLyrics.js
@@ -1,8 +1,11 @@
-//解析歌词文件-生成dom
+// => 解析歌词文件-生成dom
+
+// 误差时间(歌曲播放时与歌词显示存在误差, 修改errorTime以达到最佳显示效果)
+const errorTime = 1;
 const path = require('path');
 const fs = require('fs');
 const iconv = require('iconv-lite');
-fs.readFile(path.resolve(__dirname, './张玉华 - 原谅.lrc'), (err, data) => {
+fs.readFile(path.resolve(__dirname, './萧风 - 贝多芬的悲伤.lrc'), (err, data) => {
     let originData = iconv.decode(data, 'gbk');
     let arrayP = originData.split('\n');
     let re = ['<section class="lyric">'];
@@ -12,9 +15,9 @@ fs.readFile(path.resolve(__dirname, './张玉华 - 原谅.lrc'), (err, data) => 
         if(match){
             let text = pr.slice(10);
             if(text){
-                re.push(`  <div><p data-start="${match[0]}">${text}</p></div>`)
+                re.push(`  <div><p data-start="${correctTimeError(match[0], errorTime)}">${text}</p></div>`)
             }else{
-                re.push(`  <div style="height: 1px;"><p data-start="${match[0]}"></p></div>`)
+                re.push(`  <div style="height: 1px;"><p data-start="${correctTimeError(match[0], errorTime)}"></p></div>`)
             }
         }else{
             re.push(`  <div><p>${pr.replace(/\[/, '').replace(/\]/, '')}</p></div>`)
@@ -27,3 +30,21 @@ fs.readFile(path.resolve(__dirname, './张玉华 - 原谅.lrc'), (err, data) => 
         else console.log('lyrics.html 已保存');
     });
 });
+
+/**
+ * 纠正误差
+ * @param timeString
+ * @param timeError
+ */
+function correctTimeError(timeString, timeError) {
+    let arrTime = timeString.split(':');
+    let intTime = parseInt(arrTime[0]) * 60 + parseFloat(arrTime[1]);
+    let resultTime = intTime + timeError;
+    let minute = Math.floor(resultTime / 60);
+    let second = (resultTime % 60).toFixed(2);
+    return `${setDouble(minute)}:${setDouble(second)}`;
+}
+
+function setDouble(num) {
+    return num < 10 ? '0' + num : num
+}
