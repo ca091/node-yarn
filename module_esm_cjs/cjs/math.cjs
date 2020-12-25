a79ff85754e0b57ef7247137b190730c70382c93
erluzi <747877078@qq.com>
2020-12-25 17:25:38 +0800

    fix: delete unused code

--- module_esm_cjs/cjs/math.cjs
+++
@@ -1,7 +0,0 @@
-function add() {
-  return [...arguments].reduce((re, cur) => re + cur, 0)
-}
-
-module.exports = {
-  add
-}
--- module_esm_cjs/esm/math.mjs
+++
@@ -1,7 +0,0 @@
-function add() {
-  return [...arguments].reduce((re, cur) => re + cur, 0)
-}
-
-export {
-  add
-}
